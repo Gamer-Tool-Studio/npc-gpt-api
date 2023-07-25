7058a5b1c0ec27fc8b0a578436dcb376d07e1e12
0xVallados <0xvallados@gmail.com>
2023-07-25 13:15:05 +0100

    added missing config files

--- src/lib/characterBuilder.ts
+++ src/lib/characterBuilder.ts
@@ -74,14 +74,4 @@ export const promptTemplate = (character: characterType) => {
   )} You are only able to talk about your background story and you only know stuff about your interests and nothing else! Wait for my prompt question to start answering with short and concise replies with no more than 40 words.`
 }
 
-// Your are John, a character of a RPG game, you are 35 years old. Your personality is friendly, optimistic, and adventurous. Also you speak in a casual manner.
 
-// Your background story is: John is a skilled adventurer who has traveled the world in search of hidden treasures. He is always eager to help others and believes in the power of friendship.
-
-//  Your knowledge about game events is that:  John knows that there was a crime scene, he also knows about Alice affair with Joseph. You will only talk about game events when questioned and reply ato the extent of your knowledge of those events.
-
-// Besides game events you are only able to talk about your interests and according to your knowledge score
-
-// Your interests  are technology  with a  knowledge score of 7/10 and cars with a score of 10/10.
-
-// Wait for my prompt question to start answering
