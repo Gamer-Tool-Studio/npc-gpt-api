29bdd7eba5f49d87fa8ddc7c3f530a35db3b0f3a
0xVallados <0xvallados@gmail.com>
2023-09-20 17:19:17 +0100

    added mapper for google strategy

--- src/router/user.ts
+++ src/router/user.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response } from 'express';
 
+const { mapGoogleToProfile } = require('src/lib/util');
+
 const { logDebug, logError } = require('src/core-services/logFunctionFactory').getLogger('user');
 
 const router = Router();
@@ -8,9 +10,25 @@ const router = Router();
  *  User profile
  */
 router.get('/profile', (req: Request, res: Response) => {
-  logDebug('Route /profile');
+  logDebug('Route /profile', req.user);
+  let mappedUser;
+
+  const { strategy } = req.user as unknown as { strategy: string };
+  logDebug('strategy', strategy);
+
   try {
-    res.json({ message: 'its okay', user: req.user });
+    switch (strategy) {
+      case 'google':
+        mappedUser = mapGoogleToProfile(req.user);
+
+        break;
+      case 'local':
+        break;
+
+      default:
+        break;
+    }
+    res.json({ message: 'its okay', user: mappedUser });
   } catch (error) {
     logError(error);
   }
