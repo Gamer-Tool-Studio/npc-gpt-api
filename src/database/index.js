d1e52b945658a84d50b70ab41dbb7ed2367dc94b
0xVallados <0xvallados@gmail.com>
2023-12-11 18:17:44 +0000

    added node version

--- src/database/index.js
+++ src/database/index.js
@@ -21,14 +21,6 @@ if (!mongoDB) {
   logError('[DATABASE] ');
 }
 
-let Redis = {};
-// let RedisSubscribe = {};
-// if (process.env.USE_REDIS === 'true') {
-//   logDebug('Use REDIS try to connect : ', process.env.USE_REDIS);
-//   Redis = startRedis(config.redisUrl);
-//   // RedisSubscribe = startRedis(config.redisUrl)
-// }
-
 const Database = {
   createTokenMap: (data, options) => {
     return mongoDB.token.create(data, options);
@@ -38,16 +30,6 @@ const Database = {
   },
   createAccount: (data, options) => {
     return mongoDB.account.create(data, options);
-  },
-  createSession: (data, options) => {
-    return mongoDB.session.create(data, options);
-  },
-  findSession: (filter, select, options) => {
-    return mongoDB.session.find(filter, select, options);
-  },
-
-  findAccount: (filter, select, options) => {
-    return mongoDB.socialProof.find(filter, select, options);
   },
 
   findUser: (filter, select, options) => {
@@ -153,57 +135,6 @@ const Database = {
   findUsers: (filter, select, options) => {
     logDebug('BD Filter users ', filter);
     return mongoDB.user.find(filter, select, options);
-  },
-
-  findSignDocument: (filter, select, options) => {
-    return mongoDB.signDocument.find(filter, select, options);
-  },
-  findQesBilling: (filter, select, options) => {
-    return mongoDB.qes_billing.find(filter, select, options);
-  },
-  findEnvironment: (filter, select, options) => {
-    return mongoDB.environment.find(filter, select, options);
-  },
-  findCompanyProfile: (filter, select, options) => {
-    return mongoDB.company.find(filter, select, options);
-  },
-
-  saveToken: async (key, token, ttl) => {
-    const sTtl = ttl || config.REDIS_AUTH_TTL;
-
-    const res = await Redis.SETEX(key, sTtl, token);
-    logDebug('result after saved redis key ', res);
-  },
-
-  getToken: async (key) => {
-    logDebug('[DATA_REDIS] will get token...');
-    const token = await Redis.get(key);
-    logDebug('[DATA_REDIS] returned token ', token);
-    return token;
-  },
-
-  initKYCData: async (key, idt, reference) => {
-    const res = await Redis.hset(key, 'ref', reference, 'idt', idt, 'status', 'request.pending');
-    const ex = await Redis.expire(key, 1200); // expire after 20 minutes
-
-    logDebug('[DATA_REDIS] initKYCData result ', res, ' expire ', ex);
-  },
-
-  updateKYCData: async (key, field, data) => {
-    const res = await Redis.hset(key, field, data);
-
-    logDebug('[DATA_REDIS] updateKYCData result ', res);
-  },
-
-  getKYCData: async (key, param) => {
-    let result;
-
-    if (!param) result = await Redis.hgetall(key);
-    else result = await Redis.hget(key, param);
-
-    logDebug('[DATA_REDIS] getKYCData result ', result);
-
-    return result;
   },
 };
 
